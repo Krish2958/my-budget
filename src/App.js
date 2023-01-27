78f8e73e0a7ccf054f89124420b5d7ad5c53b07a
Krish2958 <krishm.km17@gmail.com>
2023-01-28 00:53:26 +0530

    Added General Expense Tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Button, Container, Stack } from 'react-bootstrap';
+import BudgetCard from './components/BudgetCard';
+import AddBudgetModal from './components/AddBudgetModal';
+import AddExpenseModal from './components/AddExpenseModal';
+import { useState } from 'react';
+import { useBudgets } from './contexts/BudgetsContext';
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false)
+  const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState()
+  const { budgets,getBudgetExpenses } = useBudgets()
+
+  function openAddExpenseModal(budgetId){
+    setShowAddExpenseModal(true)
+    setAddExpenseModalBudgetId(budgetId)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <Container className='my-4'>
+        <Stack direction='horizontal' gap='2' className='mb-4'>
+          <h1 className='me-auto'>Budgets</h1>
+          <Button variant='primary' onClick={() => setShowAddBudgetModal(true) }>Add Budget</Button>
+          <Button variant='outline-primary' onClick={openAddExpenseModal}>Add Expenses</Button>
+        </Stack>
+        <div style={{ display:'grid',gridTemplateColumns: "repeat(auto-fill, minmax(300px,1fr))",gap: "1rem", alignItems: "flex-start"}}></div>
+        {budgets.map(budget =>{
+          const amount= getBudgetExpenses(budget.id).reduce((total,
+            expense) => total+expense.amount,
+            0)
+          return(<BudgetCard key={budget.id} name={budget.name} amount={amount} max={budget.max} openAddExpenseClick ={() => openAddExpenseModal(budget.id) }/>
+        )})}
+          
+      </Container>
+      <AddBudgetModal show={ showAddBudgetModal } handleClose={() => setShowAddBudgetModal(false)}/>
+      <AddExpenseModal show={ showAddExpenseModal} defaultBudgetId={addExpenseModalBudgetId} handleClose={() => setShowAddExpenseModal(false)}/>
+      </>
     </div>
   );
 }
