27a2300a9bbec91be327f8cbdae443186449a025
Krish2958 <krishm.km17@gmail.com>
2023-01-28 19:32:54 +0530

    Added Hash Routing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BudgetsProvider } from "./contexts/BudgetsContext"
-
+import { HashRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <BudgetsProvider>
+      <HashRouter base='/'>
       <App />
+      </HashRouter>
     </BudgetsProvider>
   </React.StrictMode>,
   document.getElementById("root")
